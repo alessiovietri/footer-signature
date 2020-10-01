59f67726259624b60b03dca7ed89b1e48664103b
Jan Kocbek <jan.kocbek@spletna-postaja.com>
2020-10-01 12:05:29 +0200

    Slovenian language added

---
+++ lang/sl/lang.php
@@ -0,0 +1,23 @@
+<?php
+
+return [
+    'name' => 'Podpis v nogi',
+    'description' => 'Nogo prilagodite s svojim podpisom!',
+    'fields' => [
+        'title' => 'Nastavitve podpisa v nogi',
+        'text' => 'Besedilo',
+        'text_help' => 'Uporabite rezervacijsko kodo \':icon\', da postavite ikono kamorkoli v besedilu želite',
+        'random' => 'Naključna izbira ikone',
+        'random_help' => 'Vklopite za naključno izbiro ikone',
+        'selected' => 'Izbrana ikona',
+        'icons' => [
+            'title' => 'Ikone',
+            'name' => 'Ime',
+            'classes' => 'Razredi'
+        ]
+    ],
+    'settings' => [
+        'title' => 'Nastavitve podpisa v nogi',
+        'description' => 'Uredite nastavitve podpisa v nogi'
+    ]
+];
